ed389dc2432c9ac9c4798e294e32be21237505e8
gsh4124-cyber <gsh4124@gmail.com>
2026-09-02 19:31:15 +0900

    Remove superseded i18n mutation guard

--- i18n-dom-guard.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const nativeSetAttribute = Element.prototype.setAttribute;
-  Element.prototype.setAttribute = function(name, value) {
-    const next = String(value);
-    if (this.getAttribute(name) === next) return;
-    return nativeSetAttribute.call(this, name, next);
-  };
-})();
